8fb5065998b7ccd4f3c3756a9d2df3f860d140e7
Nicholas Devenish <ndevenish@gmail.com>
2025-09-22 23:19:41 +0100

    Basic connection to telnet

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,102 @@
-use telnet::{Telnet, TelnetOption, Action, Event};
+use telnet::{Action, Event, Telnet, TelnetOption};
+use std::io::{self, Write};
+use tracing::debug;
 
 fn main() {
+    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
     let mut connection = Telnet::connect(("i24-jf9mb-ctrl", 23), 256).unwrap();
-    connection.negotiate(&Action::Wont, TelnetOption::TTYPE).unwrap();
-    connection.negotiate(&Action::Wont, TelnetOption::TSPEED).unwrap();
-    connection.negotiate(&Action::Wont, TelnetOption::XDISPLOC).unwrap();
-    connection.negotiate(&Action::Wont, TelnetOption::NewEnvironment).unwrap();
-//     IAC WONT TTYPE
-// IAC WONT TSPEED
-// IAC WONT XDISPLOC
-// IAC WONT NEW-ENVIRON
+
+    // Our initial burst of expectations
+    for (action, option) in [
+        (Action::Do, TelnetOption::SuppressGoAhead),
+        (Action::Will, TelnetOption::TTYPE),
+        (Action::Will, TelnetOption::NAWS),
+        (Action::Wont, TelnetOption::NewEnvironment),
+        (Action::Wont, TelnetOption::XDISPLOC),
+    ] {
+        debug!("CTRL: Sending {action:?} {option:?}");
+        connection.negotiate(&action, option).unwrap();
+    }
     let data = loop {
         let event = connection.read().expect("Read Error");
-        if ! matches!(event, Event::Data(_)) {
-            println!("CTRL: {event:?}");
+        if !matches!(event, Event::Data(_)) {
+            debug!("CTRL: {event:?}");
         }
+
+        // let mut handled_echo = false;
         match event {
-            Event::Negotiation(Action::Do, option) => connection.negotiate(&match option {TelnetOption::Echo => Action::Wont}, TelnetOption::Echo).unwrap(),
-            Event::Negotiation(Action::Do, TelnetOption::Echo) => connection.negotiate(&Action::Wont, TelnetOption::Echo).unwrap(),
+            Event::Negotiation(Action::Do, option) => match option {
+                TelnetOption::NAWS => {
+                    debug!("CTRL:   └ Sending NAWS");
+                    connection
+                        .subnegotiate(TelnetOption::NAWS, &[0x00, 0x78, 0x00, 0x28])
+                        .unwrap()
+                }
+                TelnetOption::Echo => {
+                    let response = Action::Wont;
+                    debug!("CTRL:   └ Responding {response:?}");
+                    connection.negotiate(&response, option).unwrap()
+                }
+                TelnetOption::TSPEED => {
+                    let response = Action::Will;
+                    debug!("CTRL:   └ Responding {response:?}");
+                    connection.negotiate(&response, option).unwrap()
+                },
+                TelnetOption::LFLOW => {
+                    let response = Action::Wont;
+                    debug!("CTRL:   └ Responding {response:?}");
+                    connection.negotiate(&response, option).unwrap()
+                },
+                _ => (),
+            },
+            Event::Negotiation(Action::Will, option) => {
+                let response = match option {
+                    TelnetOption::Echo => Action::Do,
+                    TelnetOption::Status => Action::Dont,
+                    _ => continue,
+                };
 
-            Event::Negotiation(Action::Do, TelnetOption::TTYPE) => connection.negotiate(&Action::Will, TelnetOption::TTYPE).unwrap(),
-            Event::Negotiation(Action::Do, TelnetOption::NAWS) => connection.negotiate(&Action::Will, TelnetOption::NAWS).unwrap(),
-
-            Event::Negotiation(Action::Do, option) => connection.negotiate(&Action::Wont, option).unwrap(),
-            Event::Negotiation(Action::Will, TelnetOption::SuppressGoAhead) => connection.negotiate(&Action::Do, TelnetOption::SuppressGoAhead).unwrap(),
-            Event::Negotiation(Action::Will, option) => connection.negotiate(&Action::Do, option).unwrap(),
-
+                // let response = match option {
+                //     TelnetOption::SuppressGoAhead => Action::Do,
+                //     TelnetOption::Echo => Action::Do,
+                //     TelnetOption::TTYPE => Action::Do,
+                //     _ => Action::Dont,
+                // };
+                debug!("CTRL:   └ Responding {response:?}");
+                connection.negotiate(&response, option).unwrap()
+            }
+            // Event::Negotiation(Action::Wont)
+            Event::Subnegotiation(TelnetOption::TTYPE, data) if data.get(0) == Some(&1) => {
+                debug!("CTRL:   └ Responding TTYPE = xterm");
+                connection
+                    .subnegotiate(TelnetOption::TTYPE, "xterm".as_bytes())
+                    .unwrap();
+            }
+            Event::Subnegotiation(TelnetOption::TSPEED, data) if data.get(0) == Some(&1) => {
+                debug!("CTRL:   └ Responding TSPEED = 9600,9600");
+                connection
+                    .subnegotiate(TelnetOption::TSPEED, "38400,38400".as_bytes())
+                    .unwrap();
+            }
             Event::Data(d) => {
+                // First data means we have finished the opening negotiations
                 break d.into_vec();
             }
-            w => println!("Got unhandled: {w:?}"),
+            _ => continue,
         }
     };
-    println!("{}", String::from_utf8(data).unwrap());
+    print!("{}", String::from_utf8(data).unwrap());
     // Now we are past negotiation
     loop {
         let event = connection.read().expect("Read Error");
         let data = match event {
             Event::Data(d) => d.into_vec(),
             w => {
-                println!("CTRL: {w:?}");
+                print!("CTRL: {w:?}");
                 continue;
-            },
+            }
         };
         print!("{}", String::from_utf8(data).unwrap());
+        io::stdout().flush().unwrap();
     }
 }
